50772f3122531c3fa5c7499d974646499f35c6a4
Hasan Albanna <hasanbena02@gmail.com>
2024-04-01 06:54:09 +0700

    Pertemuan-4

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,73 @@
+import styles from "./Movies.module.css"
+function Movies() {
+    return(
+        <div className={styles.container}>
+            <section className={styles.movies}>
+                <h2 className={styles.movies__title}>Latest Movie</h2>
+                <div className={styles.movie__container}>
+                    <div className={styles.movie}>
+                        <img className={styles.movie__img} src="https://picsum.photos/id/1/300/400" alt="" />        
+                        <h3 className={styles.movie__title}>The Eminance in the Shadow</h3>
+                        <p className={styles.movie__date}>01 April 2024</p>
+                        </div>
+
+                        <div className={styles.movie}>
+                        <img className={styles.movie__img} src="https://picsum.photos/id/1/300/400" alt="" />        
+                        <h3 className={styles.movie__title}>The Eminance in the Shadow</h3>
+                        <p className={styles.movie__date}>01 April 2024</p>
+                        </div>
+
+                        <div className={styles.movie}>
+                        <img className={styles.movie__img} src="https://picsum.photos/id/1/300/400" alt="" />        
+                        <h3 className={styles.movie__title}>The Eminance in the Shadow</h3>
+                        <p className={styles.movie__date}>01 April 2024</p>
+                        </div>
+
+                        <div className={styles.movie}>
+                        <img className={styles.movie__img} src="https://picsum.photos/id/1/300/400" alt="" />        
+                        <h3 className={styles.movie__title}>The Eminance in the Shadow</h3>
+                        <p className={styles.movie__date}>01 April 2024</p>
+                        </div>
+
+                        <div className={styles.movie}>
+                        <img className={styles.movie__img} src="https://picsum.photos/id/1/300/400" alt="" />        
+                        <h3 className={styles.movie__title}>The Eminance in the Shadow</h3>
+                        <p className={styles.movie__date}>01 April 2024</p>
+                        </div>
+
+                        <div className={styles.movie}>
+                        <img className={styles.movie__img} src="https://picsum.photos/id/1/300/400" alt="" />        
+                        <h3 className={styles.movie__title}>The Eminance in the Shadow</h3>
+                        <p className={styles.movie__date}>01 April 2024</p>
+                        </div>
+
+                        <div className={styles.movie}>
+                        <img className={styles.movie__img} src="https://picsum.photos/id/1/300/400" alt="" />        
+                        <h3 className={styles.movie__title}>The Eminance in the Shadow</h3>
+                        <p className={styles.movie__date}>01 April 2024</p>
+                        </div>
+
+                        <div className={styles.movie}>
+                        <img className={styles.movie__img} src="https://picsum.photos/id/1/300/400" alt="" />        
+                        <h3 className={styles.movie__title}>The Eminance in the Shadow</h3>
+                        <p className={styles.movie__date}>01 April 2024</p>
+                        </div>
+
+                        <div className={styles.movie}>
+                        <img className={styles.movie__img} src="https://picsum.photos/id/1/300/400" alt="" />        
+                        <h3 className={styles.movie__title}>The Eminance in the Shadow</h3>
+                        <p className={styles.movie__date}>01 April 2024</p>
+                        </div>
+
+                        <div className={styles.movie}>
+                        <img className={styles.movie__img} src="https://picsum.photos/id/1/300/400" alt="" />        
+                        <h3 className={styles.movie__title}>The Eminance in the Shadow</h3>
+                        <p className={styles.movie__date}>01 April 2024</p>
+                        </div>
+                </div>
+            </section>
+            
+        </div>
+    )
+}
+export default Movies
