96394e3dd36d7af24b3b90c85b6301a9a5c4360d
Hasan Albanna <hasanbena02@gmail.com>
2024-06-04 18:39:24 +0700

    pertemuan-7

---
+++ src/pages/home.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import Form from "../components/Form/Form"
+import Hero from "../components/Hero/Hero"
+import Movies from "../components/Movies/Movies"
+import data from "../utils/constant/data"
+
+
+function Home() {
+    const [movies, setMovies] =  useState(data)
+    return (
+        <div>
+            <Hero />
+            <Movies movies={movies} setMovies={setMovies} />
+            <Form movies={movies} setMovies={setMovies}/>
+        </div>
+    )
+
+}
+
+export default Home
